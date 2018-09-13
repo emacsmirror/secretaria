e841f8a655e129677d81c013a9fc1234c9d16955
Jorge Araya Navarro <jorge@esavara.cr>
2018-09-13 14:42:18 -0600

    fix: Update function calls on tests
    Names were changed but test were left without update
    
    - https://gitlab.com/shackra/secretaria/-/jobs/97502461

--- test/secretaria-test.el
+++ test/secretaria-test.el
@@ -9,15 +9,15 @@
   "Should fail because the agenda file is empty."
   (let ((org-test-file-empty t))
     (within-sandbox
-     (should-not (secretaria-get-appt 'due))
-     (should-not (secretaria-get-appt 'unknown)))))
+     (should-not (secretaria-appt-get-appt 'due))
+     (should-not (secretaria-appt-get-appt 'unknown)))))
 
 (ert-deftest secretaria-due-appts ()
   "Should find due appointments."
   (within-sandbox
-   (should (equal (length (secretaria-get-appt 'due)) 3))))
+   (should (equal (length (secretaria-appt-get-appt 'due)) 3))))
 
 (ert-deftest secretaria-unknown-time ()
   "Should find appointments for today in unspecified time of day."
   (within-sandbox
-   (should (equal (length (secretaria-get-appt 'unknown)) 3))))
+   (should (equal (length (secretaria-appt-get-appt 'unknown)) 3))))
